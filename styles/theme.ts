7afe559d934ada760d48691fc4ee93ec08b9456a
Erik Almaraz <erkalmar@gmail.com>
2022-04-09 15:46:15 -0500

    Add theme's definition

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,2 +1,25 @@
 import { Theme } from "./theme.interfaces"
-export const LightMode: Theme = {}
+
+export const LightMode: Theme = {
+  name: "LightMode",
+  fontFamily: "Roboto",
+  fontColor: "#333",
+  backgroundColor: "#fff",
+  breakpoints: {
+    mobile: 369,
+    tablet: 768,
+    desktop: 960,
+  },
+}
+
+export const DarkMode: Theme = {
+  name: "DarkMode",
+  fontFamily: "Roboto",
+  fontColor: "#fff",
+  backgroundColor: "#333",
+  breakpoints: {
+    mobile: 369,
+    tablet: 768,
+    desktop: 960,
+  },
+}
